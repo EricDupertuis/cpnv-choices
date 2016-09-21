3bd18c8e0ba0ba59567b121bbbcc5993d399b26c
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-09-21 18:42:57 +0200

    added aboutcontroller and controllerinterface

---
+++ src/Cpnv/ChoicesBundle/Controller/AboutController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eric
+ * Date: 21.09.16
+ * Time: 18:36
+ */
+
+namespace Cpnv\ChoicesBundle\Controller;
+
+
+class AboutController
+{
+
+}
---
+++ src/Cpnv/ChoicesBundle/Interfaces/ControllerInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eric
+ * Date: 21.09.16
+ * Time: 18:41
+ */
+
+namespace Cpnv\ChoicesBundle\Interfaces;
+
+
+class ControllerInterface
+{
+
+}
