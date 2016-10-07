db87395cee5b09b2000031fe58b7a632e851bf25
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-10-07 15:43:18 +0200

    getDb alias, shorter for db actions

--- src/Cpnv/ChoicesBundle/Controller/AbstractController.php
+++ src/Cpnv/ChoicesBundle/Controller/AbstractController.php
@@ -10,20 +10,46 @@ use Cpnv\ChoicesBundle\Container;
  * @package Cpnv\ChoicesBundle\Controller
  * @author Eric Dupertuis <dupertuis.eric@gmail.com>
  */
-abstract class AbstractController extends Container
+abstract class AbstractController
 {
-    public function render($template, $vars)
+    /**
+     * @var Container $container
+     */
+    protected $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param $template
+     * @param null $vars
+     */
+    public function render($template, $vars = null)
     {
         try {
             TemplateEngine::render($template, $vars);
-
         } catch (\Exception $e) {
             echo 'Error rendering template :' . $template . ' ' . $e->getMessage();
         }
     }
 
+    /**
+     * @param $path
+     * @return bool
+     */
     public function redirect($path)
     {
         header('Location: ' . $path);
+        return 1;
+    }
+
+    /**
+     * @return \Cpnv\ChoicesBundle\Db\DbConnection
+     */
+    public function getDB()
+    {
+        return $this->container->getDbConnection();
     }
 }
