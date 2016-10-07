0debd8e64bb84ec3b9104a4e5c809d2734efb76a
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-10-07 15:46:59 +0200

    userController

--- src/Cpnv/ChoicesBundle/Controller/UserController.php
+++ src/Cpnv/ChoicesBundle/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace Cpnv\ChoicesBundle\Controller;
 
+use Cpnv\ChoicesBundle\Db\QueryBuilder;
+
 /**
  * Class UserController
  * @package Cpnv\ChoicesBundle\Controller
@@ -9,15 +11,44 @@ namespace Cpnv\ChoicesBundle\Controller;
  */
 class UserController extends AbstractController
 {
+    /**
+     * @return string|void
+     */
     public function indexAction()
     {
         if (isset($_POST['login']) && $_POST['login'] == true) {
             $this->redirect('/');
         }
-        $vars = [
-            'test'
-        ];
 
-        echo $this->render('login.html', $vars);
+        if (!empty($_POST)) {
+            try {
+                $this->login($_POST['email'], $_POST['password']);
+            } catch (\Exception $e) {
+                return $e->getMessage();
+            }
+        }
+
+        return $this->render('login.php', ['test' => 'doggo', 'var' => 1]);
+    }
+
+    /**
+     * @param $email
+     * @param $pass
+     */
+    private function login($email, $pass)
+    {
+        $qb = new QueryBuilder();
+        $qb->select('user')
+            ->where('email', '=', $email)
+            ->andWhere('password', '=', $pass);
+
+        $db = $this->getDB();
+        var_dump($qb->getQuery());
+        $db->execute($qb->getQuery());
+
+        $hash = password_hash($_POST['password'], PASSWORD_DEFAULT, ['cost' => 12]);
+        var_dump($hash);
+
+        //$user = $this->dbConnection->execute($qb->getQuery());
     }
 }
