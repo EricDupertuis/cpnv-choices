069fb23963c5158d41187b677c15f1bca822a5f2
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-11-11 15:27:12 +0100

    wip user class

--- app/classes/user.php
+++ app/classes/user.php
@@ -1,30 +1,43 @@
 <?php
 
+namespace Kingdom;
+
 /**
  * Class User
  */
 class User
 {
+    const ROLE_ADMIN = 'ROLE_ADMIN';
+
+    const ROLE_USER = 'ROLE_USER';
+
     private $username;
 
     private $email;
 
-    private $permissions;
+    private $permissions = [];
+
+    private $password;
 
     /**
-     * @var PDO $db
+     * @var \PDO $db
      */
     private $db;
 
     /**
      * User constructor.
-     * @param PDO $db
+     * @param \PDO $db
      */
-    public function __construct(PDO $db)
+    public function __construct(\PDO $db)
     {
         $this->db = $db;
     }
 
+    /**
+     * @param $username
+     * @param $password
+     * @return bool
+     */
     public function connect($username, $password)
     {
         $stmt = $this->db->prepare('SELECT * users WHERE username=:username');
@@ -37,7 +50,7 @@ class User
             return false;
         }
 
-        if ($this->checkHash($password, $user['password'])) {
+        if (password_verify($password, $user['password'])) {
             $_SESSION['user'] = $user['username'];
             $_SESSION['password'] = $user['password'];
 
@@ -48,22 +61,71 @@ class User
     }
 
     /**
-     * @param $password
-     * @param $hashedPassword
-     * @return bool
+     * @param array $data
      */
-    private function checkHash($password, $hashedPassword)
+    public function register($data)
     {
-        if (password_verify($password, $hashedPassword)) {
-            return true;
-        }
+        $stmt = $this->db->prepare('
+            INSERT INTO \'user\' (username, email, password, permissions)
+            VALUES (:username, :email, :password, :permissions)
+        ');
 
-        return false;
+        if (isset($data['username']) && $data['username'] != '') {
+
+        }
     }
 
     public function checkPermissions()
     {
 
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPermissions()
+    {
+        return $this->permissions;
+    }
+
+    /**
+     * @param mixed $permissions
+     */
+    public function setPermissions($permissions)
+    {
+        $this->permissions = $permissions;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * @param mixed $salt
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
     }
 
     /**
