e40deaaba3ea1471fc9611de0fe0cad4c7a89965
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-12-15 00:26:47 +0100

    bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+// set all configurations in an array
+$config = [];
+
+$config['app'] = require_once('config/app.php');
+$config['routes'] = require_once('config/routing.php');
+$config['db'] = require_once('config/database.php');
+
+// includes main class
+include_once './classes/User.php';
+include_once './classes/App.php';
+
+session_start();
+
+// set db connection
+$db = new PDO(
+    'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['dbName'] . ';charset=utf8mb4',
+    $config['db']['user'],
+    $config['db']['password']
+);
+
+// manage user
+$app = new \Kingdom\App($config);
+$user = new Kingdom\User($db);
+
