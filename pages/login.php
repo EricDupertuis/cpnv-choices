f448bd60fb0b8324f05097b70e7c00f6da6ff7d8
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-12-15 23:56:10 +0100

    add subpages divs

--- pages/login.php
+++ pages/login.php
@@ -1,13 +1,15 @@
 <?php include_once 'incs/head.php' ?>
-<div class="container">
-    <form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
-        <label for="username">Username</label>
-        <input type="text" id="username" name="username">
+    <div class="container">
+        <div class="souspage">
+            <form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username">
 
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password">
 
-        <input type="submit" value="submit">
-    </form>
-</div>
-<?php include_once 'incs/footer.php'?>
+                <input type="submit" value="submit">
+            </form>
+        </div>
+    </div>
+<?php include_once 'incs/footer.php' ?>
--- pages/register.php
+++ pages/register.php
@@ -1,16 +1,18 @@
 <?php include_once 'incs/head.php' ?>
-<form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
-    <div class="container">
-        <label for="username">Username</label>
-        <input type="text" id="username" name="username">
+    <form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
+        <div class="container">
+            <div class="souspage">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username">
 
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email">
 
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password">
 
-        <input type="submit" value="submit">
-    </div>
-</form>
+                <input type="submit" value="submit">
+            </div>
+        </div>
+    </form>
 <?php include_once 'incs/footer.php'; ?>
