e5f48d4e2a04661d874ae04a84ba8dd6fb843755
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-12-15 23:54:59 +0100

    almost there

--- export.php
+++ export.php
@@ -8,9 +8,17 @@ if (isset($_POST['export']) && $_POST['export'] == 'ok') {
 
     $qs = $query->fetchAll();
 
-    foreach ($qs as $q) {
-        
-    }
-}
+    $output = fopen("php://output",'w') or die("Can't open php://output");
+    header("Content-Type:application/csv");
+    header("Content-Disposition:attachment;filename=questions-export" . date('Y-m-d') . ".csv");
 
-include_once 'pages/export.php';
+    fputcsv($output, array('question1','question2'));
+
+    foreach ($qs as $q) {
+        var_dump($q);
+        $return = [$q['answer_one'], $q['answer_two']];
+        fputcsv($output, $return);
+    }
+} else {
+    include_once 'pages/export.php';
+}
--- pages/export.php
+++ pages/export.php
@@ -1,2 +1,11 @@
-<?php
-
+<?php include_once 'incs/head.php'; ?>
+    <div class="container-fluid">
+        <div class="souspage">
+            <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" enctype="multipart/form-data">
+                Export:
+                <input type="hidden" value="ok" name="export">
+                <input type="submit" value="submit" name="submit">
+            </form>
+        </div>
+    </div>
+<?php include_once 'incs/footer.php';
