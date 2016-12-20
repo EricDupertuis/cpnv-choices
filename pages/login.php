4d8b21cf6475c116f8b8cc912b7b8570141fd746
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-12-20 23:21:21 +0100

    login and register styling

--- pages/login.php
+++ pages/login.php
@@ -1,15 +1,36 @@
-<?php include_once 'incs/head.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Kingdom</title>
+    <link href="/public/style.css" rel="stylesheet">
+    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <script
+        src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+        crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="wrap">
     <div class="container-fluid">
-        <div class="souspage">
-            <form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username">
+        <div class="main">
+            <p class="log-logo">
+                <img id="logo" src="../public/images/kingdom-logo.png" alt="Logo - Kingdom">
+            </p>
+            <div class="log">
+                <h1>Login</h1>
+                <form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
+                    <label for="username">Nom d'utilisateur</label>
+                    <input type="text" id="username" name="username">
 
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password">
+                    <label for="password">Mot de passe</label>
+                    <input type="password" id="password" name="password">
 
-                <input type="submit" value="submit">
-            </form>
+                    <input type="submit" value="submit">
+                </form>
+            </div>
         </div>
     </div>
 <?php include_once 'incs/footer.php' ?>
--- pages/register.php
+++ pages/register.php
@@ -1,18 +1,38 @@
-<?php include_once 'incs/head.php' ?>
-    <form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Kingdom</title>
+    <link href="/public/style.css" rel="stylesheet">
+    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <script
+        src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+        crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="wrap">
         <div class="container-fluid">
-            <div class="souspage">
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username">
+            <div class="main">
+                <p class="log-logo">
+                    <img id="logo" src="../public/images/kingdom-logo.png" alt="Logo - Kingdom">
+                </p>
+                <div class="log">
+                    <h1>Inscription</h1>
+                    <form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
+                        <label for="username">Username</label>
+                        <input type="text" id="username" name="username">
 
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email">
 
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password">
 
-                <input type="submit" value="submit">
+                        <input type="submit" value="submit">
+                    </form>
+                </div>
             </div>
-        </div>
-    </form>
 <?php include_once 'incs/footer.php'; ?>
