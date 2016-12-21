845510c54bfec4554c32f9969aaec35869fb7b42
Eric Dupertuis <dupertuis.eric@gmail.com>
2016-12-21 01:21:30 +0100

    stats page

--- pages/stats.php
+++ pages/stats.php
@@ -1,7 +1,9 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: eric
- * Date: 21.12.16
- * Time: 00:58
- */
+<?php include_once 'incs/head.php'; ?>
+    <div class="souspage">
+        <div class="container">
+            <h3>Nombres de questions : <?php echo $nbQuestions; ?></h3>
+            <h3>Nombre d'utilisateurs : <?php echo $nbUsers; ?></h3>
+            <h3>Utilisateurs actifs : <?php echo $activeUsers; ?></h3>
+        </div>
+    </div>
+<?php include_once 'incs/footer.php';
--- stats.php
+++ stats.php
@@ -1,2 +1,24 @@
 <?php
 
+include_once 'bootstrap.php';
+
+if (!$user->isLogged()) {
+    $app->redirect('');
+}
+
+$query = $db->prepare('SELECT COUNT(*) FROM questions_sets;');
+$query->execute();
+
+$nbQuestions = $query->fetch()[0];
+
+$query = $db->prepare('SELECT count(*) FROM users;');
+$query->execute();
+
+$nbUsers = $query->fetch()[0];
+
+$query = $db->prepare('SELECT user_id FROM users_answers GROUP BY user_id;');
+$query->execute();
+
+$activeUsers = count($query->fetchAll());
+
+include_once $app->getConfig()['app']['app_dir'].'pages/stats.php';
